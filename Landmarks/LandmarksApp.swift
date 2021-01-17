44763f99657a4358623c4113a137007039d9dad7
Evidence Rex Osikhena <rexsunon.dev@gmail.com>
2021-01-17 15:20:31 +0100

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by Evidence Osikhena on 17/01/2021.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
